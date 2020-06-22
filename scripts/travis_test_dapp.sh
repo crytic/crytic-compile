c8be8fa259d9a3cf2c5548fe0617a006a77296e6
Josselin <josselin@trailofbits.com>
2020-06-22 10:49:24 +0200

    Update Dapp test

--- scripts/travis_test_dapp.sh
+++ scripts/travis_test_dapp.sh
@@ -9,12 +9,7 @@ git config --global user.email "ci@trailofbits.com"
 git config --global user.name "CI User"
 
 
-curl https://nixos.org/nix/install | sh
-. "$HOME/.nix-profile/etc/profile.d/nix.sh"
-nix-env -iA nixpkgs.cachix
-cachix use dapp
-git clone --recursive https://github.com/dapphub/dapptools $HOME/.dapp/dapptools
-nix-env -f $HOME/.dapp/dapptools -iA dapp seth solc hevm ethsign
+curl https://dapp.tools/install | sh
 
 dapp init
 
@@ -24,15 +19,4 @@ then
     echo "dapp test failed"
     exit -1
 fi
-# TODO: for some reason dapp output is not deterministc
 
-#cd -
-#
-#DIFF=$(diff /tmp/dapp/crytic-export/contracts.json tests/expected/dapp-demo.json)
-#if [ "$DIFF" != "" ]
-#then
-#    echo "Dapp test failed"
-#    echo $DIFF
-#    exit -1
-#fi
-#
