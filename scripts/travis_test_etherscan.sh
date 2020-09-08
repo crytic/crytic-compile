ca5f9b45bc178d9eb088c21df8a9f0c096d32473
Josselin <josselin@trailofbits.com>
2020-09-08 12:53:49 +0200

    Minor fix

--- scripts/travis_test_etherscan.sh
+++ scripts/travis_test_etherscan.sh
@@ -8,7 +8,7 @@ cd /tmp/etherscan  || exit 255
 wget -O solc-0.4.25 https://github.com/ethereum/solidity/releases/download/v0.4.25/solc-static-linux
 chmod +x solc-0.4.25
 
-crytic-compile 0x7F37f78cBD74481E593F9C737776F7113d76B315 --compile-remove-metadata --solc "./solc-0.4.25" --etherscan-apikey $GITHUB_ETHERSCAN
+crytic-compile 0x7F37f78cBD74481E593F9C737776F7113d76B315 --compile-remove-metadata --solc "./solc-0.4.25" --etherscan-apikey "$GITHUB_ETHERSCAN"
 
 if [ $? -ne 0 ]
 then
@@ -16,7 +16,7 @@ then
     exit 255
 fi
 
-crytic-compile rinkeby:0xFe05820C5A92D9bc906D4A46F662dbeba794d3b7 --compile-remove-metadata --solc "./solc-0.4.25"  --etherscan-apikey $GITHUB_ETHERSCAN
+crytic-compile rinkeby:0xFe05820C5A92D9bc906D4A46F662dbeba794d3b7 --compile-remove-metadata --solc "./solc-0.4.25"  --etherscan-apikey "$GITHUB_ETHERSCAN"
 
 if [ $? -ne 0 ]
 then
