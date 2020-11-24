18b77d166c084b0e8de7df9718d542ced50f45ad
Josselin <josselin@trailofbits.com>
2020-11-24 14:19:37 +0100

    Run black 19.10b0

--- crytic_compile/platform/buidler.py
+++ crytic_compile/platform/buidler.py
@@ -61,8 +61,7 @@ class Buidler(AbstractPlatform):
             cmd = base_cmd + ["compile"]
 
             LOGGER.info(
-                "'%s' running",
-                " ".join(cmd),
+                "'%s' running", " ".join(cmd),
             )
 
             process = subprocess.Popen(
