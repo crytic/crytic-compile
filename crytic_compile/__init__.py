f2a8952d066cb3f6eb443ec3650edd3d406fb3a4
Josselin <josselin@trailofbits.com>
2021-04-09 17:21:13 +0200

    Minor

--- crytic_compile/__init__.py
+++ crytic_compile/__init__.py
@@ -3,6 +3,7 @@ Init module
 """
 
 from .crytic_compile import CryticCompile, compile_all, is_supported
+from .compilation_unit import CompilationUnit
 from .cryticparser import cryticparser
 from .platform import InvalidCompilation
 from .utils.zip import save_to_zip
