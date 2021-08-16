cf4247c9ffed8aaf3c770dbeb7eb6a4ada30f921
Josselin <josselin@trailofbits.com>
2021-08-16 18:51:41 +0200

    0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Util to facilitate smart contracts compilation.",
     url="https://github.com/crytic/crytic-compile",
     author="Trail of Bits",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     python_requires=">=3.6",
     install_requires=["pysha3>=1.0.2"],
