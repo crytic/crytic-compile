6980c9ab2a073400e950e3a03990ac348e03e490
Gronw D <gronw.d@outlook.com>
2022-07-04 14:05:44 +0800

    optimistic support

--- crytic_compile/platform/etherscan.py
+++ crytic_compile/platform/etherscan.py
@@ -34,6 +34,7 @@ ETHERSCAN_BASE_BYTECODE = "https://%s/address/%s#code"
 SUPPORTED_NETWORK = {
     # Key, (prefix_base, perfix_bytecode)
     "mainet:": (".etherscan.io", "etherscan.io"),
+    "optim:": ("-optimistic.etherscan.io", "optimistic.etherscan.io"),
     "ropsten:": ("-ropsten.etherscan.io", "ropsten.etherscan.io"),
     "kovan:": ("-kovan.etherscan.io", "kovan.etherscan.io"),
     "rinkeby:": ("-rinkeby.etherscan.io", "rinkeby.etherscan.io"),
