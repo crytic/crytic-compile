91e91e252a500fd2ae64c70165f842c0aa786414
Emilio López <emilio.lopez@trailofbits.com>
2022-12-12 17:37:43 -0300

    foundry: add test for extra_output_files

--- scripts/ci_test_foundry.sh
+++ scripts/ci_test_foundry.sh
@@ -17,9 +17,24 @@ mkdir forge_test
 cd forge_test || exit 255
 forge init
 
+# base test: just a "forge init" project
+
 crytic-compile .
 if [ $? -ne 0 ]
 then
-    echo "foundry test failed"
+    echo "foundry base test failed"
+    exit 255
+fi
+
+
+# extra files test: "forge init" but with extra_output_files enabled (issue #296)
+forge clean
+echo >> foundry.toml # missing newline at the end of template
+echo 'extra_output_files = ["metadata", "abi"]' >> foundry.toml
+
+crytic-compile .
+if [ $? -ne 0 ]
+then
+    echo "foundry extra files test failed"
     exit 255
 fi
