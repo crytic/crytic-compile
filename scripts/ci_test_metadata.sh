36d73e91e47ab82566579993b7be2728d8519c46
rmi7 <user@rmi7.me>
2022-12-21 12:59:45 +0100

    fix

--- scripts/ci_test_metadata.sh
+++ scripts/ci_test_metadata.sh
@@ -9,7 +9,7 @@ solc-select use 0.5.12 --always-install
 
 # We'll fetch eh DAI contract and parse it's metadata
 echo "from crytic_compile import CryticCompile" > metadata_test.py
-echo "crytic_compile_instance = CryticCompile('0x6B175474E89094C44Da98b954EedeAC495271d0F',etherscan_api_key=$GITHUB_ETHERSCAN)" >> metadata_test.py
+echo "crytic_compile_instance = CryticCompile('0x6B175474E89094C44Da98b954EedeAC495271d0F',etherscan_api_key=${GITHUB_ETHERSCAN})" >> metadata_test.py
 echo "cu = list(crytic_compile_instance.compilation_units.values())[0]" >> metadata_test.py
 echo "metadata_keys = cu.metadata_of('Dai').keys()" >> metadata_test.py
 echo "metadata_key1_val = cu.metadata_of('Dai')['bzzr1']" >> metadata_test.py
