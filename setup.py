727cd9189fcc3968688461e0e91083972a718327
Feist Josselin <josselin.feist@gmail.com>
2023-01-11 17:49:53 +0100

    0.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="Util to facilitate smart contracts compilation.",
     url="https://github.com/crytic/crytic-compile",
     author="Trail of Bits",
-    version="0.2.5",
+    version="0.3.0",
     packages=find_packages(),
     python_requires=">=3.8",
     install_requires=["pycryptodome>=3.4.6", "cbor2"],
