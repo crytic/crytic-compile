9a0cb863be366ee9b07d5287be1ca3415e991c51
alpharush <0xalpharush@protonmail.com>
2023-08-07 10:50:57 -0500

    pylint

--- tests/test_library_linking.py
+++ tests/test_library_linking.py
@@ -2,8 +2,8 @@
 Test library linking
 """
 import re
-import pytest
 from pathlib import Path
+import pytest
 from crytic_compile.crytic_compile import CryticCompile
 
 TEST_DIR = Path(__file__).resolve().parent
@@ -12,6 +12,7 @@ LIBRARY_PLACEHOLDER_REGEX = r"__.{36}__"
 
 
 def test_library_linking() -> None:
+    """Test that the placeholder is not present in the bytecode when the libraries are provided"""
     cc = CryticCompile(
         Path(TEST_DIR / "library_linking.sol").as_posix(),
         compile_libraries="(NeedsLinkingA, 0xdead),(NeedsLinkingB, 0x000000000000000000000000000000000000beef)",
@@ -25,7 +26,6 @@ def test_library_linking() -> None:
                 len(re.findall(r"__.{36}__", source_unit.bytecode_runtime("TestLibraryLinking")))
                 == 2
             )
-            # Test that the placeholder is not present in the bytecode when the libraries are provided
             libraries = compilation_unit.crytic_compile.libraries
             assert (
                 len(
@@ -46,8 +46,9 @@ def test_library_linking() -> None:
 
 
 def test_library_linking_validation() -> None:
+    """Test that invalid compile libraries argument raises an error"""
     with pytest.raises(ValueError):
-        cc = CryticCompile(
+        CryticCompile(
             Path(TEST_DIR / "library_linking.sol").as_posix(),
             compile_libraries="(NeedsLinkingA, 0x)",
         )
